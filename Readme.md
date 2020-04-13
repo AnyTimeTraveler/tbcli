6b3a1b64662f7218c7128eb9fa1dcbb24e758c7e
Simon Struck <simon@simonscode.org>
2020-04-13 04:40:32 +0200

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,53 @@
+# Telegram Bot Command Line Interface
+
+This handy tool should allow you to use a telegram bot like a logger.
+
+### Arguments:
+
+`-s` or `--send-only` immediately exits as soon as there is nothing more to send (stdin closes)
+
+`-i` or `--id` sets the id of the person/group/channel you want to send the message to.
+Can also be supplied via environment variable: `TELEGRAM_RECEIVER_ID`
+
+`-t` or `--token` sets the token for the bot you are using.
+Can also be supplied via environment variable: `TELEGRAM_BOT_TOKEN`
+
+# Examples
+
+__(These assume that you are using the environment variables to set the receiver and bot token!)__
+
+## Sending a message
+
+You can use this to pipe anything into the bot:
+- It will send a message per new line.
+
+```bash
+% echo "Test" | tgcli --send-only
+```
+
+To be informed of any server errors on a development box.
+
+```bash
+% tail -f /var/log/nginx/access.log | grep 503 | tgcli --send-only
+```
+
+## Receiving messages
+
+Print them to the command line:
+- One message per line.
+- Ignores non-text messages.
+
+```bash
+% tbcli < /dev/null
+USERNAME,FIRST_NAME,LAST_NAME,USER_ID,CHAT_ID,EPOCH_SECONDS,MESSAGE_TEXT
+```
+
+Ignore all but the message:
+```bash
+% tbcli < /dev/null | cut -d, -f7
+``` 
+
+# Known Bugs
+
+1. In receive-mode, the input and output race with each other, so sending and receiving at the same time is problematic.
+   Best to use `< /dev/null` to close the input stream, so the output stream has the lock guaranteed.
---
+++ src/main.rs
@@ -0,0 +1,108 @@
+#![feature(async_closure)]
+
+use std::{env, io};
+use std::io::Write;
+
+use argparse::{ArgumentParser, Store, StoreTrue};
+use futures::{join, StreamExt};
+use telegram_bot::*;
+
+struct Options {
+    id: String,
+    token: String,
+    send_only: bool,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let mut options = Options {
+        id: "".to_string(),
+        token: "".to_string(),
+        send_only: false,
+    };
+
+    {  // this block limits scope of borrows by ap.refer() method
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Use telegram bot from cli.");
+        ap.refer(&mut options.id).add_option(&["-i", "--id"], Store, "ID of receiving user/group/channel");
+        ap.refer(&mut options.token).add_option(&["-t", "--token"], Store, "Telegram API KEY");
+        ap.refer(&mut options.send_only).add_option(&["-s", "--send-only"], StoreTrue, "Send only");
+        ap.parse_args_or_exit();
+    }
+
+    if options.token.is_empty() {
+        options.token = env::var("TELEGRAM_BOT_TOKEN").expect("TELEGRAM_BOT_TOKEN not set");
+    }
+    if options.id.is_empty() {
+        options.id = env::var("TELEGRAM_RECEIVER_ID").expect("TELEGRAM_RECEIVER_ID not set");
+    }
+
+    let api = Api::new(options.token.as_str());
+    if options.send_only {
+        Ok(send(api.clone(), &options).await?)
+    } else {
+        let (r_send, r_receive) = join!(
+            send(api.clone(), &options),
+            receive(api.clone())
+        );
+        if r_send.is_err() {
+            r_send
+        } else if r_receive.is_err() {
+            r_receive
+        } else {
+            Ok(())
+        }
+    }
+}
+
+async fn send(api: Api, options: &Options) -> Result<(), Error> {
+    let user_id: i64 = match options.id.parse() {
+        Ok(value) => value,
+        Err(_) => {
+            eprintln!("Invalid id!");
+            return Ok(());
+        }
+    };
+    let stdin = io::stdin();
+    let user_id: UserId = user_id.into();
+    let mut input = String::new();
+    loop {
+        match stdin.read_line(&mut input) {
+            Ok(0) => return Ok(()),
+            Ok(_) => {
+                if !input.is_empty() {
+                    api.send(user_id.text(&input)).await?;
+                }
+                input.clear();
+            }
+            Err(error) => {
+                eprintln!("error: {}", error);
+                return Ok(());
+            }
+        }
+    }
+}
+
+async fn receive(api: Api) -> Result<(), Error> {
+    let mut stream = api.stream();
+    let mut stdout = io::stdout();
+
+    while let Some(update) = stream.next().await {
+        let update = update?;
+        if let UpdateKind::Message(message) = update.kind {
+            if let MessageKind::Text { ref data, .. } = message.kind {
+                let string = format!("{},{},{},{},{},{},{}\n",
+                                     &message.from.username.unwrap_or("".to_string()),
+                                     &message.from.first_name,
+                                     &message.from.last_name.unwrap_or("".to_string()),
+                                     &message.from.id,
+                                     &message.chat.id(),
+                                     &message.date,
+                                     data);
+                stdout.write(string.as_bytes()).expect("BBB");
+                stdout.flush().expect("AAA")
+            }
+        }
+    }
+    Ok(())
+}
